03a06edbea53a61593d0a59f9e6cc5d5284522a1
tahirainam <cattblackwhite@gmail.com>
2025-09-08 17:08:08 +0530

    modified navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="nav">
+    <nav className="navbar w-full flex items-center justify-between p-2 bg-gray-800 text-white">
+      <div className="logo text-1xl font-bold">
+        <p>Tahira Inam</p>
+      </div>
+      <div className="nav-links">
         <ul>
-          <li><a href="#home"> home </a></li>
-          <li><a href="#about"> about </a></li>
-          <li><a href="#projects"> projects </a></li>
-          <li>
-            <a 
-              href="path_to_your_resume.pdf" 
-              download="Your_Name_Resume.pdf" 
-              className="btn"
-            >
-              resume
-            </a>
-          </li>
+          <li><a href="#home"> Home </a></li>
+          <li><a href="#about"> About </a></li>
+          <li><a href="#projects"> Projects </a></li>
+        </ul>
+      </div>
+      <div className="social-media flex space-x-4 text-1xl">
+        <ul>
+          <li><a href="https://www.linkedin.com/in/tahirainam" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></a></li>
+          <li><a href="https://github.com/tahirainam" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a></li>
+          <li><a href="https://medium.com/tahirainam" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-medium"></i></a></li>
         </ul>
       </div>
     </nav>
