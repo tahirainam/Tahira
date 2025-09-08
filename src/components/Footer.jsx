47511b542d96d12ab082e77a48d4234600b24184
tahirainam <cattblackwhite@gmail.com>
2025-09-08 16:43:44 +0530

    modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,8 @@ function Footer() {
   return (
     <div className="footer">
       <footer>
-        <p>&copy; 2023–{new Date().getFullYear()} ✦ Tahira Inam ✦</p>
-        <div className="social-icons">
-          <a href="https://github.com/Inahira" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
-          <a href="mailto:contact.inahira@gmail.com" target="_blank" rel="noopener noreferrer"><i className="fas fa-envelope"></i></a>
-          <a href="https://linkedin.com/in/Inahira_" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin-in"></i></a>
-        </div>
+        <p>Built and designed by Tahira Inam</p>
+        <p>&copy; 2023-{new Date().getFullYear()}</p>
       </footer>
     </div>
   );
