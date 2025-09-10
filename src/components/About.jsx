3a786bec342789052fc614a25624cc64166d16b2
tahirainam <cattblackwhite@gmail.com>
2025-09-10 11:36:06 +0530

    upadted about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,16 +7,16 @@ function About() {
       {/* About Me */}
       <div className="max-w-3xl mb-12">
         <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6">
-          About
+          About Me
         </h2>
         <p className="text-sm sm:text-base md:text-lg leading-relaxed text-gray-700">
-          Hi there, it's <span className="underline">Tahira Inam</span>, a{" "}
-          <strong>Software Engineering</strong> student with a knack for solving problems.  
-          I love to spend time solving <strong>Leetcode problems</strong>.  
-          <br />
-          With 1+ year of experience in{" "}
-          <strong>Front-End Development</strong>, I enjoy creating clean and functional UIs.  
-          When I’m not coding, I’m exploring <strong>Astronomy</strong>. ✨
+          Hi, I’m Tahira Inam. I’m a <span className="underline">Software Engineering student</span>, learning my way through front-end, back-end, and a bit of creative coding. Along the way, I’ve done <span className="underline">remote internships</span>, built small projects, and tried things just to see how they work.
+        </p>
+        <p className="text-sm sm:text-base md:text-lg leading-relaxed text-gray-700">
+          Right now, I’m focused on <span className="underline">growing my skills</span> and becoming the kind of developer who builds things that are simple, useful, and a little creative.
+        </p>
+        <p className="text-sm sm:text-base md:text-lg leading-relaxed text-gray-700">
+         <br />Outside of coding, I spend time sketching and exploring new ideas. I’m also really fascinated by space, robotics, and how science keeps shaping the world.
         </p>
       </div>
 
@@ -26,22 +26,24 @@ function About() {
           Languages & Tools
         </h2>
         <p className="text-sm sm:text-base mb-6 text-gray-600">
-          Here are some technologies I’ve worked with:
+          Here are some technologies I’ve been working with:
         </p>
 
         <div className="flex flex-wrap justify-center gap-2 sm:gap-3">
           {[
+            "JavaScript",
+            "React.js",
+            "Three.js",
             "HTML5",
             "CSS3",
-            "JavaScript ES6",
-            "Bootstrap",
-            "WordPress",
-            "Responsive Web Design",
             "Python",
+            "Tailwind CSS",
+            
             "C++",
             "Git",
             "GitHub",
             "VS Code",
+            
           ].map((skill) => (
             <span
               key={skill}
