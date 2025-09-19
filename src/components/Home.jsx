3b1eabf04fb3a6f3e3ae859256b7257962f00018
tahirainam <cattblackwhite@gmail.com>
2025-09-19 17:19:29 +0530

    updated Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
         <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-6xl mb-4 leading-snug max-w-[90%] sm:max-w-[80%]">
   Hey, I'm <span className="text-gray-900 font-bold">Tahira Inam</span>.
 </h1>
-
+<p>⦿ Available for small to medium sized projects</p>
 <h2 className="mb-3 text-xl sm:text-3xl md:text-3xl lg:text-3xl text-gray-700 font-light max-w-[90%] sm:max-w-[75%]" id="subheading">
   I like making code look good and work even better.
 </h2>
@@ -29,7 +29,7 @@ function Home() {
             href="mailto:tahirainam1@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex items-center text-black px-4 sm:px-6 py-2 sm:py-3 border border-gray-400 bg-white hover:border-black text-base sm:text-lg font-medium transition"
+            className="inline-flex items-center text-black px-8 sm:px-6 py-2 sm:py-3 border border-gray-400 bg-white hover:border-black text-base sm:text-lg font-medium transition"
           >
             <i className="fas fa-envelope mr-2"></i> say hi
           </a>
